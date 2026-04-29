da65f14e1ad406eac91892aa7404f4ab0830adf9
Your Name <you@example.com>
2026-04-29 14:47:35 +0530

    Update welcome page title to reflect application name for improved branding consistency.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title></title>
+        <title>{{ config('app.name', 'Laravel') }}</title>
     </head>
     <body class="min-h-screen bg-[#121212]"></body>
 </html>
